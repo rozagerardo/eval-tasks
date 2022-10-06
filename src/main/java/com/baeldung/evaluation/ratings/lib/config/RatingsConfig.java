20c6d2f13594b82d079299742b6384db2893b30a
Ger Roza <rozagerardo@gmail.com>
2022-10-06 18:26:37 -0300

    First approach - not working correctly; mixes reviews references (revewableId is not dependent of the table)

---
+++ src/main/java/com/baeldung/evaluation/ratings/lib/config/RatingsConfig.java
@@ -0,0 +1,10 @@
+package com.baeldung.evaluation.ratings.lib.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigurationPackage(basePackages = "com.baeldung.evaluation.ratings.lib")
+public class RatingsConfig {
+
+}
