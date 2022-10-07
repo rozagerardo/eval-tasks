eea7abd0b3635fe3749041d50282c9c450d4e12f
Ger Roza <rozagerardo@gmail.com>
2022-10-06 21:18:12 -0300

    Fixed error in review FKs mixing up target classes

--- src/main/java/com/baeldung/evaluation/ratings/lib/domain/ReviewableEntity.java
+++ src/main/java/com/baeldung/evaluation/ratings/lib/domain/ReviewableEntity.java
@@ -1,24 +1,37 @@
 package com.baeldung.evaluation.ratings.lib.domain;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 
 @MappedSuperclass
-public class ReviewableEntity implements Reviewable {
+public abstract class ReviewableEntity implements Reviewable, Serializable {
+
+	private static final long serialVersionUID = 1622357429438514992L;
+
+	private final String reviewableType = this.getClass().getName();
 
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinColumn(name = "reviewableId")
-	private List<Review> reviews;
+//	@JoinColumn(name = "reviewableId")
+	@JoinColumns({ @JoinColumn(name = "reviewableId", referencedColumnName = "id"),
+			@JoinColumn(name = "type", referencedColumnName = "reviewableType") })
+	private List<Review> reviews = new ArrayList<>();
 
 	@Override
 	public List<Review> getReviews() {
 		return this.reviews;
+	}
+
+	public String getReviewableType() {
+		return reviewableType;
 	}
 
 	@Override
